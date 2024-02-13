e15a5519d23dff17ed119a87ed2eaf0a57a47f6e
nadar <nadar.veselov@mail.ru>
2024-02-13 23:42:40 +0300

    initial commit

--- carousel.js
+++ carousel.js
@@ -1,29 +1,64 @@
-const SLIDES_PER_VIEW = 3
+const DEFAULT_CAROUSEL_OPTIONS = {
+    desktop: {
+        slidesPerView: 3,
+    },
+    mobile: {
+        slidesPerView: 1,
+    },
+}
+
+NodeList.prototype.toggleAttribute = function (name, force) {
+    for (const el of this.values()) {
+        el.toggleAttribute(name, force);
+    }
+}
+
+NodeList.prototype.addEventListener = function (name, cb) {
+    for (const el of this.values()) {
+        el.addEventListener(name, cb);
+    }
+}
+
+NodeList.prototype.setTextContent = function (content) {
+    for (const el of this.values()) {
+        el.textContent = content;
+    }
+}
 
 class Carousel {
-    constructor(id) {
+    constructor(id, options) {
         this.currentPage = 1;
+
+        this.options = {...DEFAULT_CAROUSEL_OPTIONS, ...options};
 
         this.rootEl = document.querySelector(`[data-carousel="${id}"]`);
         this.innerEl = this.rootEl.getElementsByClassName('carousel__inner')[0];
         this.slideEls = this.rootEl.getElementsByClassName('carousel__slide');
 
-        this.prevBtnEl = document.querySelector(`[data-carousel-prev="${id}"]`);
-        this.nextBtnEl = document.querySelector(`[data-carousel-next="${id}"]`);
-        this.currentPageEl = document.querySelector(`[data-carousel-current-page="${id}"]`);
-        this.pagesCountEl = document.querySelector(`[data-carousel-pages-count="${id}"]`);
+        this.prevBtnEls = document.querySelectorAll(`[data-carousel-prev="${id}"]`);
+        this.nextBtnEls = document.querySelectorAll(`[data-carousel-next="${id}"]`);
+        this.currentPageEls = document.querySelectorAll(`[data-carousel-current-page="${id}"]`);
+        this.pagesCountEls = document.querySelectorAll(`[data-carousel-pages-count="${id}"]`);
 
-        this.updateDetails();
+        this.paginationEls = document.querySelectorAll(`[data-carousel-pagination="${id}"]`);
+        this.mediaQueryList = window.matchMedia("(max-width: 480px)");
+
+        this.renderPagination();
+        this.update();
         this.updateSlidesWidth();
         this.registerEventListeners();
     }
 
-    get slideWidth() {
-        return this.rootEl.clientWidth / SLIDES_PER_VIEW;
+    get isMobile() {
+        return this.mediaQueryList.matches;
+    }
+
+    get slidesPerView() {
+        return this.isMobile ? this.options.mobile.slidesPerView : this.options.desktop.slidesPerView;
     }
 
     get pagesCount() {
-        return Math.ceil(this.slideEls.length / SLIDES_PER_VIEW);
+        return Math.ceil(this.slideEls.length / this.slidesPerView);
     }
 
     get isFirstPage() {
@@ -34,40 +69,71 @@ class Carousel {
         return this.currentPage === this.pagesCount;
     }
 
-    updateSlidesWidth() {
-        for (const slide of this.slideEls) {
-            slide.style.width = `${this.slideWidth}px`;
-        }
-    }
-
     next() {
         if (this.isLastPage) return;
 
-        this.to(++this.currentPage);
+        this.to(this.currentPage + 1);
     }
 
     prev() {
         if (this.isFirstPage) return;
 
-        this.to(--this.currentPage);
+        this.to(this.currentPage - 1);
     }
 
     to(page) {
-        this.innerEl.style.transform = `translateX(${-this.innerEl.clientWidth * (page - 1)}px)`;
+        this.currentPage = page;
 
-        this.updateDetails();
+        this.innerEl.style.transform = `translateX(${-100 * (page - 1)}%)`;
+
+        this.update();
     }
 
-    updateDetails() {
-        this.currentPageEl.textContent = this.currentPage;
-        this.pagesCountEl.textContent = this.pagesCount.toString();
+    update() {
+        this.currentPageEls.setTextContent(this.currentPage);
+        this.pagesCountEls.setTextContent(this.pagesCount.toString());
+        this.prevBtnEls.toggleAttribute('disabled', this.isFirstPage);
+        this.nextBtnEls.toggleAttribute('disabled', this.isLastPage);
 
-        this.prevBtnEl.toggleAttribute('disabled', this.isFirstPage);
-        this.nextBtnEl.toggleAttribute('disabled', this.isLastPage);
+        for (const el of this.paginationEls) {
+            for (let bulletIdx = 0; bulletIdx < el.children.length; bulletIdx++) {
+                el.children.item(bulletIdx).classList.toggle('carousel__bullet--active', this.currentPage === bulletIdx + 1);
+            }
+        }
     }
 
     registerEventListeners() {
-        this.prevBtnEl.addEventListener('click', this.prev.bind(this));
-        this.nextBtnEl.addEventListener('click', this.next.bind(this));
+        this.mediaQueryList.addEventListener('change', () => {
+            this.updateSlidesWidth();
+
+            this.to(1);
+        });
+
+        this.prevBtnEls.addEventListener('click', this.prev.bind(this));
+        this.nextBtnEls.addEventListener('click', this.next.bind(this));
+    }
+
+    updateSlidesWidth() {
+        const targetWidth = `${100 / this.slidesPerView}%`;
+
+        for (const slide of this.slideEls) {
+            slide.style.width = targetWidth;
+        }
+    }
+
+    renderPagination() {
+        if (!this.paginationEls.length) return;
+
+        for (const el of this.paginationEls) {
+            el.innerHTML = '';
+
+            for (let page = 1; page <= this.pagesCount; page++) {
+                const bullet = document.createElement('div');
+                bullet.classList.add('carousel__bullet');
+                bullet.classList.toggle('carousel__bullet--active', page === 1);
+
+                el.appendChild(bullet);
+            }
+        }
     }
 }
